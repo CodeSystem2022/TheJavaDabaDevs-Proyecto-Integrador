f118c864ac0b9a9b19a8577f69e9bfa584993ef7
Marita <113071029+vrunito2021@users.noreply.github.com>
2023-06-27 22:57:50 -0300

    Update JavaDabaClub.py
    Subiendo la Base de Datos

--- JavaDabaClub-Proyect/JavaDabaClub.py
+++ JavaDabaClub-Proyect/JavaDabaClub.py
@@ -11,4 +11,25 @@ def registrar_socio():
     direccion = entry_direccion.get()
     mail = entry_mail.get()
     telefono = entry_telefono.get()
-    
+    # Conexión a la base de datos MySQL
+    try:
+        db = mysql.connector.connect(
+            host="127.0.0.1",
+            user="root",
+            password="1234",
+            database="socios"
+        )
+        cursor = db.cursor()
+
+        # Inserta los datos del socio en la tabla correspondiente
+        query = "INSERT INTO socios (nombre, apellido, dni, direccion, mail, telefono) VALUES (%s, %s, %s, %s, %s, %s)"
+        values = (nombre, apellido, dni, direccion, mail, telefono)
+        cursor.execute(query, values)
+        db.commit()
+
+        # Cierra la conexión a la base de datos
+        cursor.close()
+        db.close()
+
+        # Muestra un mensaje de éxito
+        messagebox.showinfo("Registro Exitoso", "El socio ha sido registrado exitosamente.") 
