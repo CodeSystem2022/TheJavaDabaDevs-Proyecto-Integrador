27b657014444d4b8e8aca9a279de9ae223fcd21b
Marcelo Alejandro Boujón <77926233+boujonmarcelo@users.noreply.github.com>
2023-06-27 23:03:41 -0300

    Update JavaDabaClub.py
    Se subió código para Proyecto Integrador

--- JavaDabaClub-Proyect/JavaDabaClub.py
+++ JavaDabaClub-Proyect/JavaDabaClub.py
@@ -11,4 +11,21 @@ def registrar_socio():
     direccion = entry_direccion.get()
     mail = entry_mail.get()
     telefono = entry_telefono.get()
+
+ #Código subido por Marcelo
     
+ # Limpia los campos de entrada
+        entry_nombre.delete(0, tk.END)
+        entry_apellido.delete(0, tk.END)
+        entry_dni.delete(0, tk.END)
+        entry_direccion.delete(0, tk.END)
+        entry_mail.delete(0, tk.END)
+        entry_telefono.delete(0, tk.END)
+    except mysql.connector.Error as error:
+        # Muestra un mensaje de error en caso de que ocurra un problema con la base de datos
+        messagebox.showerror("Error de Base de Datos", f"No se pudo registrar el socio.\nError: {error}")
+
+# Función para buscar un socio por su nombre en la base de datos
+def buscar_socio():
+    # Obtiene el nombre ingresado por el usuario
+    nombre_buscar = entry_buscar.get()
